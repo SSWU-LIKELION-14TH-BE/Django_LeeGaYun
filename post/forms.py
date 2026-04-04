133d52675686ebcda9f35a9bc4a36ceb3fd33373
riveryunny <gayunphone@gmail.com>
2026-04-05 03:47:38 +0900

    feat: 좋아요, 댓글, 대댓글 기능 구현

--- post/forms.py
+++ post/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Post
+from .models import Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -10,4 +11,12 @@ class PostForm(forms.ModelForm):
             'content': forms.Textarea(attrs={'class': 'post-textarea', 'placeholder': '내용'}),
             'tech_stack': forms.TextInput(attrs={'class': 'post-input', 'placeholder': '기술 스택 선택'}),
             'github_link': forms.TextInput(attrs={'class': 'post-input', 'placeholder': '깃허브 링크'}),
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 2, 'placeholder': '댓글을 입력하세요', 'class': 'comment-input'}),
         }
--- post/views.py
+++ post/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .forms import PostForm
-from .models import Post
+from .forms import PostForm, CommentForm
+from .models import Post, Comment
 
 @login_required
 def post_create_view(request):
@@ -18,4 +20,50 @@ def post_create_view(request):
 
 def post_detail_view(request, pk):
 	post = get_object_or_404(Post, pk=pk)
-	return render(request, 'post_detail.html', {'post': post})
+	comments = Comment.objects.filter(post=post, parent__isnull=True).order_by('-created_at')
+	comment_form = CommentForm()
+
+	# 댓글/대댓글 작성 처리
+	if request.method == 'POST' and 'comment_submit' in request.POST:
+		comment_form = CommentForm(request.POST)
+		if comment_form.is_valid():
+			new_comment = comment_form.save(commit=False)
+			new_comment.post = post
+			new_comment.author = request.user
+			parent_id = request.POST.get('parent_id')
+			if parent_id:
+				try:
+					parent_comment = Comment.objects.get(id=parent_id, post=post)
+					new_comment.parent = parent_comment
+				except Comment.DoesNotExist:
+					new_comment.parent = None
+			new_comment.save()
+			return redirect('post_detail', pk=post.pk)
+
+	return render(request, 'post_detail.html', {
+		'post': post,
+		'comments': comments,
+		'comment_form': comment_form,
+	})
+
+# 좋아요 처리 뷰
+@login_required
+def post_like_view(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	user = request.user
+	if user in post.likes.all():
+		post.likes.remove(user)
+	else:
+		post.likes.add(user)
+	return HttpResponseRedirect(reverse('post_detail', args=[pk]))
+
+# 댓글 좋아요 처리 뷰
+@login_required
+def comment_like_view(request, comment_id):
+	comment = get_object_or_404(Comment, id=comment_id)
+	user = request.user
+	if user in comment.likes.all():
+		comment.likes.remove(user)
+	else:
+		comment.likes.add(user)
+	return redirect('post_detail', pk=comment.post.pk)
